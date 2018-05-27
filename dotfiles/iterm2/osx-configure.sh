7ccd22968e59e932f006f50be6dd3928c02a40b5
TempleCloud <tim.langford@gmail.com>
2018-05-27 12:06:30 +0100

    Added dotfiles for iTerm2.

---
+++ dotfiles/iterm2/osx-configure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# References
+#
+# http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+
+# Configure iTerm2
+#
+function configure() {
+    local _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    # Specify the preferences directory.
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${_dir}"
+    # Tell iTerm2 to use the custom preferences in the directory.
+    defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+}
+
+configure
