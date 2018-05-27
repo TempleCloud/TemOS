6cb03ddd355e6dd1530437fe4c46d3501a869e66
TempleCloud <tim.langford@gmail.com>
2018-05-27 16:06:33 +0100

    Added temos init script.

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set e
+
+# Export TEMOS_HOME. This should be the 'temos' directory.
+export TEMOS_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Export TEMOS_BIN. Create directroy if it does not exist.
+export TEMOS_BIN="${TEMOS_HOME}/bin"
+if [ ! -d "${TEMOS_BIN}" ]; then
+    echo "temos 'bin' directory does not exist. Creating ${TEMOS_BIN}."
+    mkdir -p "${TEMOS_BIN}"
+fi
