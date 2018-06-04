d6fcbe62aaa7c66be5dad53c77e230db11f6e052
TempleCloud <tim.langford@gmail.com>
2018-06-04 23:28:44 +0100

    Fixed typo.

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set e
 # Export TEMOS_HOME. This should be the 'temos' directory.
 export TEMOS_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Export TEMOS_BIN. Create directroy if it does not exist.
+# Export TEMOS_BIN. Create directory if it does not exist.
 export TEMOS_BIN="${TEMOS_HOME}/bin"
 if [ ! -d "${TEMOS_BIN}" ]; then
     echo "temos 'bin' directory does not exist. Creating ${TEMOS_BIN}."
