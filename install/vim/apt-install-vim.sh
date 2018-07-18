310fc966829642c871762e26c1c05bac54ba4a53
Tim Langford <tim.langford@gmail.com>
2018-07-18 23:44:07 +0100

    Added apt vim installer.

---
+++ install/vim/apt-install-vim.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install Vim.
+#
+sudo apt-get install -y vim
---
+++ install/vim/brew-install-macvim.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install macvim via homebrew.
+#
+brew install macvim --env-std --with-override-system-vim
