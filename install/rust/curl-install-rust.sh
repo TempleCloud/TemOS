7c0548d4ab766af3ee4e73a67cfb0d093f5b4419
Tim Langford <tim.langford@gmail.com>
2018-09-03 09:28:14 +0100

    Install rustup, rust and cargo.

---
+++ install/rust/curl-install-rust.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# References
+#
+# https://www.rust-lang.org/en-US/install.html
+
+curl https://sh.rustup.rs -sSf | sh
+
+# rustup install nightly
+# rustup run nightly cargo run
