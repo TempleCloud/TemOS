5f532b24be692740decf0f519d8934b8d425e68e
Tim Langford <tim.langford@gmail.com>
2018-09-03 09:33:23 +0100

    Modernized.

--- install/vscode/apt-install-vscode.sh
+++ install/vscode/apt-install-vscode.sh
@@ -9,7 +9,7 @@ function install() {
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' 
-    sudo apt-get update
-    sudo apt-get install -y code
+    sudo apt update
+    sudo apt install -y code
 } && install 
 
