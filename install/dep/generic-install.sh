61f2e669383d8b9720826b58127d6dc3e43682ed
Tim Langford <tim.langford@gmail.com>
2018-09-12 00:48:53 +0100

    Added golang dep install.

---
+++ install/dep/generic-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://golang.github.io/dep/
+# https://github.com/golang/dep 
+
+# Moving executable to /home/temple/Work/dev/golang/bin/dep
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
