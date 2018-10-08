aec42df4494c9b3894bb1a9af117f983b376d54a
Timothy Langford <timothy.langford@oracle.com>
2018-10-08 13:27:23 +0100

    Added config template example.

--- install/oci/oci.sh
+++ install/oci/oci.sh
@@ -8,12 +8,26 @@ export OCI_CONFIG="${OCI_HOME}/config"
 # https://docs.us-phoenix-1.oraclecloud.com/Content/API/Concepts/apisigningkey.htm
 # https://docs.cloud.oracle.com/iaas/Content/API/Concepts/sdkconfig.htm
 
+
 function setup() {
     oci setup
 }
 
+
 function setup-repair() {
     oci setup repair-file-permissions --file ${OCI_CONFIG}
+}
+
+
+function gen-config-template() {
+    cat > oci-config-template <<EOF
+[DEFAULT]
+user=ocid1.user.oc1.longguid
+key_file=/Users/YOU/.oci/oci_api_key.pem
+fingerprint=some:cool:hex:code
+tenancy=ocid1.tenancy.oc1..your.tenancy
+region=us-ashburn-1
+EOF
 }
 
 
