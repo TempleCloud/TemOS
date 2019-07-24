ede72d6925e18792441a6a6df2487a7ad29ec2ac
Tim Langford <tim.langford@gmail.com>
2019-07-25 00:21:59 +0100

    Adding sysstat installer.

---
+++ install/sysstat/apt-install-sysstat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# mpstat - Report processors related statistics.
+# iostat - Report Central Processing Unit (CPU) statistics and input/output statistics for devices and partitions.
+
+function install() {
+    sudo apt install sysstat
+}
+
