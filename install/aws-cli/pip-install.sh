82200ff14dc7543285d411933230042ecf3aaf19
Tim Langford <tim.langford@gmail.com>
2019-08-01 09:10:26 +0100

    Fixed AWS CLI install.

--- install/aws-cli/pip-install.sh
+++ install/aws-cli/pip-install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# AWS CLI              - https://aws.amazon.com/cli/
+# Install AWS CLI      - https://docs.aws.amazon.com/cli/latest/userguide/install-linux.html
+# Troubleshoot AWS CLI - https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-troubleshooting.html
+
 function install() {
     pip install awscli
+    chmod +x "$HOME/.local/bin/aws"
     echo "Ensure '$HOME/.local/bin' is on your path."
 }
 
