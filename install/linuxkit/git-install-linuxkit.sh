968acb08e362a6ea8dd34c793a64c036bc15b082
Tim Langford <tim.langford@gmail.com>
2019-08-17 12:09:43 +0100

    Added linuxkit install.

---
+++ install/linuxkit/git-install-linuxkit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://github.com/linuxkit/linuxkit
+# https://collabnix.com/top-10-reasons-why-linuxkit-is-better-than-the-traditional-os-distribution/
+
+function install() {
+    git clone git@github.com:linuxkit/linuxkit.git
+    pushd linuxkit
+    # NB: Builds in a docker container. Requires docker.
+    make
+    # qemu
+    cp ./bin/linuxkit ${TEMOS_BIN}/linuxkit
+    popd
+    rm -Rf linuxkit
+} && install
