dbe32ddbed44809306cca65dffb611413d69544d
Tim Langford <tim.langford@gmail.com>
2019-08-19 09:11:07 +0100

    Added expect install.

---
+++ install/expect/apt-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Expect - A small language for scripts that require user interactions.
+# https://likegeeks.com/expect-command/
+
+function install() {
+    sudo apt install expect
+} && install
