f868db2a6221c32a7de46440b002b60f5f6ac057
Timothy R J Langford <tim.langford@gmail.com>
2019-12-07 16:59:39 +0000

    Updating ubunut install script.

--- install/powerline/ubuntu-bash-install-powerline-fonts.sh
+++ install/powerline/ubuntu-bash-install-powerline-fonts.sh
@@ -3,15 +3,13 @@
 # https://github.com/powerline/fonts
 
 function install() {
-    # Install fonts.
-    echo "Installing powerline fonts..."
+    # install fonts
     git clone https://github.com/powerline/fonts.git --depth=1
     cd fonts
     ./install.sh
     cd ..
     rm -rf fonts
-    # Configure fonts.
-    echo "Configuring powerline fonts..."
+    # install user configuration file.
     local fconfig="${HOME}/.config/fontconfig/conf.d"
     if [ ! -d "${fconfig}" ]; then
         mkdir -p "${fconfig}"
@@ -20,10 +18,7 @@ function install() {
     curl -sLO https://raw.githubusercontent.com/powerline/fonts/master/fontconfig/50-enable-terminess-powerline.conf
     fc-cache -vf
     popd
-    # Enable 'bitmap' fonts.
-    echo "Enabling powerline bitmap fonts..."
+    # enable 'bitmap' fonts.
     sudo rm -f /etc/fonts/conf.d/70-no-bitmaps.conf
     sudo ln -sf /etc/fonts/conf.avail/70-yes-bitmaps.conf /etc/fonts/conf.d/70-yes-bitmaps.conf
-    # Final Configuration
-    echo "Remember to configure your terminal and browser to use: 'Meslo LG S for Powerline Regular'"
 } && install
