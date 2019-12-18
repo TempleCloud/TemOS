cae3ccae064f02577c149a0d9c44382b100a75e2
Timothy R J Langford <tim.langford@gmail.com>
2019-12-18 20:43:21 +0000

    Updated repo fetcher.

--- dotfiles/git/update-repos.sh
+++ dotfiles/git/update-repos.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
 
 function update_repo() {
-    local repo_name=$1
+    local org_name=$1
+    local repo_name=$2
     if [ -d "${repo_name}" ]; then
         echo "Updating ${repo_name}..."
         pushd ${repo_name} > /dev/null
         git pull
         popd > /dev/null
     else
-        echo "Cloning ${repo_name}..."
-        git clone git@github.com:tc-playground/${repo_name}.git
+        echo "Cloning ${org_name}/${repo_name}..."
+        git clone git@github.com:${org_name}/${repo_name}.git
     fi
 }
 
-# Core Respositories
-update_repo embedded-playground
-update_repo os-playground
-update_repo linux-playground
-update_repo infosec-playground
-update_repo nw-playground
-update_repo cloud-playground
+function update_tc-playground() {
+    local org_name="tc-playground"
+    local target_dir=${HOME}/Work/dev/github.com/${org_name}
 
-# Language Repositories
-update_repo language-playground
-update_repo c-playground
-update_repo rust-playground
-update_repo java-playground
-update_repo erlang-playground
-update_repo crafting-interpreters
+    mkdir -p ${target_dir}
 
-# General Development
-update_repo development-playground
-update_repo kubernetes-playground
+    pushd ${target_dir} > /dev/null
+    
+    # Core Respositories
+    update_repo ${org_name} embedded-playground
+    update_repo ${org_name} os-playground
+    update_repo ${org_name} linux-playground
+    update_repo ${org_name} infosec-playground
+    update_repo ${org_name} nw-playground
+    update_repo ${org_name} cloud-playground
+    # Language Repositories
+    update_repo ${org_name} language-playground
+    update_repo ${org_name} c-playground
+    update_repo ${org_name} rust-playground
+    update_repo ${org_name} java-playground
+    update_repo ${org_name} erlang-playground
+    update_repo ${org_name} crafting-interpreters
+    # General Development
+    update_repo ${org_name} development-playground
+    update_repo ${org_name} kubernetes-playground
+    # Web Repositories
+    update_repo ${org_name} web-playground
+    update_repo ${org_name} flutter-playground
+    update_repo ${org_name} FlutterCasts
 
-# Web Repositories
-update_repo web-playground
-update_repo flutter-playground
-update_repo FlutterCasts
+    # Data Science
+    update_repo ${org_name} machine-learning-playground
 
-# Data Science
-update_repo machine-learning-playground
+    popd > /dev/null
+}
+
+update_tc-playground
+
 
