d72d9fd5c36d489b87afe131c8eafeeb29e4693d
Tim Langford <tim.langford@gmail.com>
2020-01-18 14:11:35 +0000

    Updating notes on virtualbox install for debian with secure boot enabled.

--- install/virtualbox/debian-apt-install.sh
+++ install/virtualbox/debian-apt-install.sh
@@ -13,18 +13,42 @@ function install() {
 
     sudo apt update
     sudo apt install linux-headers-$(uname -r) dkms
-    sudo apt install virtualbox-6.0 
+    sudo apt install virtualbox-6.0
 
     # Install Extension Pack
     #
+    wget http://download.virtualbox.org/virtualbox/6.0.0/Oracle_VM_VirtualBox_Extension_Pack-6.0.0.vbox-extpack
+
+    # Get Guest Additions and Manual
+    #
+    wget http://download.virtualbox.org/virtualbox/6.0.0/VBoxGuestAdditions_6.0.0.iso
+    wget http://download.virtualbox.org/virtualbox/6.0.0/UserManual.pdf
+
+    # Sign vboxdrv
+    #
+    # 1. If required generate a cert.
+    openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=turing-kernel-mod-signer/"
+    
+    # 2. Sign the required modules
+    sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vboxdrv)
+    
+    # Check the module has been signed
+    # tail $(sudo modinfo -n vboxdrv) | grep "Module signature appended"
+    
+    # 3.Enroll the key and specify password.
+    sudo mokutil --import MOK.der
+
+    # Rebobt machine and specify password.
+
+    # Check the key is enrolled
+    mokutil --test-key MOK.der
+
     # wget https://download.virtualbox.org/virtualbox/6.0.0/Oracle_VM_VirtualBox_Extension_Pack-6.0.0.vbox-extpack
     
     # Sign 'vboxdrv' kernel module  / Reconfigure
     #
-    # * https://askubuntu.com/questions/777308/virtualbox-problem-kernel-module-is-not-loaded
     # * https://askubuntu.com/questions/760671/could-not-load-vboxdrv-after-upgrade-to-ubuntu-16-04-and-i-want-to-keep-secur/768310#768310
-    #
-    # sudo /sbin/vboxconfig
+
 
 
 
