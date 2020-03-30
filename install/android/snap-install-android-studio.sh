961092302c9d7c55463b0c0f806947b7bf4688f4
Tim Langford <tim.langford@gmail.com>
2020-03-30 19:22:21 +0100

    Updated installer.

--- install/android/snap-install-android-studio.sh
+++ install/android/snap-install-android-studio.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Android Studio - Run Device
+
 function install() {
     sudo snap install android-studio --classic
+    sudo apt install adb
+    sudo usermod -aG plugdev $USER
 } && install
