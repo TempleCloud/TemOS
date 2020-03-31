35143ca40325a8247ec8cfc4293fb4146ea43bfe
Tim Langford <tim.langford@gmail.com>
2020-03-31 21:34:27 +0100

    Added goland.

---
+++ install/goland/linux-install.sh
@@ -0,0 +1,9 @@
+
+
+curl -LO https://download.jetbrains.com/go/goland-2019.1.1.tar.gz goland-2019.1.1.tar.gz
+
+https://download.jetbrains.com/go/goland-2019.1.1.tar.gz.sha256
+
+tar xfz goland-2019.1.1.tar.gz goland-2019.1.1
+
+sudo tar xfz goland-2017.3.tar.gz -C /opt/
