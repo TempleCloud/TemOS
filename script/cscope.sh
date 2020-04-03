03c58dfdf60dd40679c08a885ef770039a14cb8c
Tim Langford <tim.langford@gmail.com>
2020-04-03 17:12:20 +0100

    Added cscope notes.

---
+++ script/cscope.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# function index() {
+#     [ ! -d "${PWD}/.nav" ]; mkdir -p "${PWD}/.nav"
+#     ctags -R -f ${PWD}/.nav/tags .
+#     cscope -R
+# }
+# 
+# NB: <ctrl-d> to exit.
+# alias scope="cscope -R"
+
+# alias tag="ctags -R -f ./.ctags/tags ."
+
